c7530ff5b408a833227be57b24dd8af6990c1c67
Sindt <czindt@gmail.com>
2017-09-29 13:46:01 +0200

    added query param to rest

--- systemintegration.loanbroker/loanbroker-receiver/src/main/java/loanbroker/receiver/boundary/QuoteRequestResource.java
+++ systemintegration.loanbroker/loanbroker-receiver/src/main/java/loanbroker/receiver/boundary/QuoteRequestResource.java
@@ -1,11 +1,11 @@
 package loanbroker.receiver.boundary;
 
+import javax.json.Json;
 import javax.json.JsonObject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -13,14 +13,17 @@ import loanbroker.receiver.controller.QouteRequestController;
 
 @Path("quote")
 @Produces(MediaType.APPLICATION_JSON)
-@Consumes(MediaType.APPLICATION_JSON)
 public class QuoteRequestResource {
 
-	QouteRequestController controller = new QouteRequestController();
+	private QouteRequestController controller = new QouteRequestController();
 
-	@GET
+	@POST
 	@Path("send")
-	public Response getQuoteRequest(@FormParam("message") JsonObject input) {
+	public Response getQuoteRequest(@QueryParam("ssn") long ssn, @QueryParam("amount") int amount,
+			@QueryParam("duration") int duration) {
+
+		JsonObject input = Json.createObjectBuilder().add("ssn", ssn).add("amount", amount).add("duration", duration)
+				.build();
 		return controller.sendMessageBasic(input).build();
 	}
 
